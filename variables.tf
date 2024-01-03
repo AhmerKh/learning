ac1fb29c7a65269a3f2e17adea414ba7259765b4
ahmer <ahmer.khalid@hotmail.com>
2024-01-03 15:55:30 +0500

    First Commit

---
+++ variables.tf
@@ -0,0 +1,30 @@
+variable "vpc_id" {
+  default = "<vpc-id>"
+}
+
+variable "aws_account_id" {
+  default = "<account-id>"
+}
+
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "image_repo_name" {
+  default = "pipe-demo"
+}
+variable "image_tag" {
+  default = "latest"
+}
+variable "image_repo_url" {
+  default = "<account-id>.dkr.ecr.<region>.amazonaws.com/<repository>"
+}
+variable "github_repo_owner" {
+  default = "AhmerKh"
+}
+variable "github_repo_name" {
+  default = "terraform-ecs-codepipeline-pipe-demo"
+}
+variable "github_branch" {
+  default = "main"
+}
